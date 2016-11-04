9cbf17e52eca3a7c5f14f08ff181bfa72f965a3a
wanglaiqi <1191580068@qq.com>
2016-11-04 21:03:25 +0800

    add the new code about git knowledge

--- LeetCodeHashTable/HashTableProblem.cpp
+++ LeetCodeHashTable/HashTableProblem.cpp
@@ -7,6 +7,8 @@ class HashTableSolution
 public:
 	HashTableSolution();
 	~HashTableSolution();
+	int cal(int x);
+	int fun(int x, int y);
 
 private:
 
@@ -20,8 +22,35 @@ HashTableSolution::~HashTableSolution()
 {
 }
 
+int HashTableSolution::cal(int x)
+{
+	if (x < 2)
+	{
+		return x;
+	}
+	return cal(x - 1) + cal(x - 2);
+}
+
+int HashTableSolution::fun(int x, int y)
+{
+	int z = x % y;
+	if (z == 0)
+	{
+		return y;
+	}
+	else
+	{
+		return fun(y,z);
+	}
+}
+
 int main()
 {
+	HashTableSolution solution;
+	//int result = solution.cal(6);
+	//cout << result << endl;
+	int result = solution.fun(12,8);
+	cout << result << endl;
 	system("pause");
 	return 0;
 }
